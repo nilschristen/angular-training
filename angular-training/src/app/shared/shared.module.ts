ea994867902bbafde3a4a080df687c92815a86ff
Nils Christen <nils.christen3@gmai.com>
2022-07-02 15:44:09 +0200

    added base layout of home.component.html renewed navigation

--- angular-training/src/app/shared/shared.module.ts
+++ angular-training/src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ValidationErrorsComponent} from './components/validation-errors/validation-errors.component';
 import {ValidationErrorComponent} from './components/validation-error/validation-error.component';
-import {NavigationComponent} from './components/navigation/navigation/navigation.component';
+import {NavigationComponent} from './components/navigation/navigation.component';
 import {RouterModule} from "@angular/router";
 import {ReactiveFormsModule} from "@angular/forms";
 
--- angular-training/src/app/video-player/containers/video-player/video-player.component.ts
+++ angular-training/src/app/video-player/containers/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-video-player',
@@ -7,38 +7,16 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 })
 export class VideoPlayerComponent implements OnInit {
 
-  @ViewChild("videoPlayer", { static: false }) videoplayer: ElementRef;
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
-  name = "Angular";
-  isPlay: boolean = false;
-  toggleVideo(event: any) {
-
-    this.videoplayer.nativeElement.play();
-  }
   playPause() {
     var myVideo: any = document.getElementById("my_video_1");
     if (myVideo.paused) myVideo.play();
     else myVideo.pause();
-  }
-
-  makeBig() {
-    var myVideo: any = document.getElementById("my_video_1");
-    myVideo.width = 560;
-  }
-
-  makeSmall() {
-    var myVideo: any = document.getElementById("my_video_1");
-    myVideo.width = 320;
-  }
-
-  makeNormal() {
-    var myVideo: any = document.getElementById("my_video_1");
-    myVideo.width = 420;
   }
 
   skip(value) {
